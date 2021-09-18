a2c63e27fa01e5a8b0a9bb9030b337a55f4c197e
Shivanshu Purohit <42869065+ShivanshuPurohit@users.noreply.github.com>
2021-09-18 12:28:08 +0530

    Update vae.py

--- VAEs/vae.py
+++ VAEs/vae.py
@@ -1,7 +1,6 @@
 import jax.numpy as jnp
 from jax import random
 from jax.util import safe_map
-from flax.core import freeze, unfreeze
 from flax import linen as nn
 from einops import repeat, rearrange
 
